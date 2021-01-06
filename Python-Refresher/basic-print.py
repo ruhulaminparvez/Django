31ea3f6c2e337534ffc312ccce0c1fc1c79c8a9c
Ruhul12 <ruhulaminparvez007@gmail.com>
2021-01-07 00:45:13 +0600

    Basics Of Pythons :rocket:

---
+++ Python-Refresher/basic-print.py
@@ -0,0 +1,11 @@
+"""Basic Variable, String Int And Print"""
+
+age = 33
+
+name = 'Ruhul'
+
+print(age)
+print(name)
+
+#Print with f-string
+print(f'My name is {name}, Age is {age}.')
---
+++ Python-Refresher/task-1.py
@@ -0,0 +1,13 @@
+'''I have given you two variables, name and age. Use the format() function to create a sentence that reads:
+
+Hi my name is julie and I am 42 years old
+
+Set that equal to the variable called sentence'''
+
+name = "Julie"
+
+age = "42"
+
+sentence = f"Hi my name is {name} and I am {age} years old"
+
+print(sentence)
