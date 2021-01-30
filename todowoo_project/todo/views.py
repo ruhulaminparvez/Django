15d1b35ac9b8d4e076d7f6c2a7330285a5e291b7
Ruhul12 <ruhulaminparvez007@gmail.com>
2021-01-30 21:15:48 +0600

    Logged In Setup For User :snake:

--- todowoo_project/todo/views.py
+++ todowoo_project/todo/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 def home(request):
     return render(request, 'todo/home.html')
@@ -26,11 +26,24 @@ def signupuser(request):
             return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
 
 def currenttodos(request):
-    return render(request, 'tosdo/currenttodos.html')
+    return render(request, 'todo/currenttodos.html')
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'] , password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form':AuthenticationForm(), 'error':'Username and password did not match'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
 
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
+
+
     
    
--- todowoo_project/todowoo/urls.py
+++ todowoo_project/todowoo/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
 
     #Auth
     path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
 
     #Todo
